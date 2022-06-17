8828094eed9206ac74dbebedf375b78538b3485e
Genossenschaft Feuer&Bohne <98707988+feuerundbohne@users.noreply.github.com>
2022-06-17 12:05:05 +0200

    Update settings.py

--- feuerundbohne/settings.py
+++ feuerundbohne/settings.py
@@ -51,6 +51,7 @@ DATABASES = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # location of your overriding templates
         'OPTIONS': {
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
