2f2b517435f6bf6cfa9f7718ddd616a6d538a916
Genossenschaft Feuer&Bohne <98707988+feuerundbohne@users.noreply.github.com>
2022-11-22 20:15:49 +0100

    Update settings.py

--- feuerundbohne/settings.py
+++ feuerundbohne/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'adminsortable2',
     'feuerundbohne',
     'polymorphic',
-    'juntagrico_assignment_request',
+    'juntagrico_assignment_request'
 ]
 
 ROOT_URLCONF = 'feuerundbohne.urls'
